bed25b8ebbadd14250a6a4ee0856b84d365562bc
Robinson-Gong <67446513+Robinson-Gong@users.noreply.github.com>
2020-09-05 13:34:32 +0900

    Update Margin and K.py

--- Margin and K.py
+++ Margin and K.py
@@ -5,33 +5,21 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import Getdata
-
-def readdata(path, tscode, format):
-    path = path.strip()
-    path = path.rstrip("\\")
-    isExists = os.path.exists(path)
-    if not isExists:
-        print('file does not exist')
-    else:
-        csv_filepath = path + '\\' + tscode + '\\'+ tscode + '_' + format +'.csv'
-        csv_data = pd.read_csv(csv_filepath)
-        return csv_data
+import datetime
 
 
-
-
-
-path = 'F:\stock data\\'
+path = 'F:\stock data'
 tscode = '002602.SZ'
-ds = '20200201'
-de = '20200828'
-GD = Getdata.getdata(ds = ds,de = de,tscode = tscode,path = path)
+ds = '20200801'
+de = '20200902'
+
+
+asset = 'E'
+format = 'margin'
+GD = Getdata.getdata(ds = ds,de = de,tscode = tscode,path = path, asset = asset)
 GD.getmargindata()
 GD.getkdata()
-
-format = 'margin'
-dataframe = readdata(path, tscode, format)
-temp = dataframe.columns
+dataframe = GD.readdata(format)
 
 plt.subplot(3,2,1)
 dataframe['rzye'] = dataframe['rzye'].astype(float)
@@ -42,13 +30,16 @@ plt.subplot(3,2,3)
 plt. plot(dataframe['trade_date'], dataframe['rqye'], 'b')
 
 format = 'k'
-dataframe = readdata(path, tscode, format)
+dataframe = GD.readdata(format)
 plt.subplot(3,2,5)
 plt.plot(dataframe['trade_date'], dataframe['close'], 'k', dataframe['trade_date'], dataframe['open'], 'r', dataframe['trade_date'], dataframe['high'], 'b')
 
+
+
 tscode = 'SZSE'
 format = 'margin'
-dataframe = readdata(path, tscode, format)
+asset = 'I'
+dataframe = GD.readdata(format)
 
 plt.subplot(3,2,2)
 dataframe['rzye'] = dataframe['rzye'].astype(float)
@@ -59,8 +50,7 @@ plt.subplot(3,2,4)
 plt. plot(dataframe['trade_date'], dataframe['rqye'], 'b')
 
 format = 'k'
-dataframe = readdata(path, tscode, format)
+dataframe = GD.readdata(format)
 plt.subplot(3,2,6)
 plt.plot(dataframe['trade_date'], dataframe['close'], 'k', dataframe['trade_date'], dataframe['open'], 'r', dataframe['trade_date'], dataframe['high'], 'b')
 plt.show()
-
